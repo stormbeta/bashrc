931ee7422bc4b53215ba7d9323984b07bc9c5337
Doug Borg <doug.borg@dougborg.org>
2013-10-26 22:46:29 -0600

    Specify tty device to avoid stty errors.

--- home/.bashrc.d/topics/stty.sh
+++ home/.bashrc.d/topics/stty.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 # Use ^X instead of ^S to stop control flow so I can reclaim ^S for forward
 # common history search
-stty -ixon
-stty stop ^X
+stty -F /dev/tty -ixon
+stty -F /dev/tty stop ^X
