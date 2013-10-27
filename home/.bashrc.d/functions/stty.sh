a540be941bc33c888cff4c0347387fd5976eb827
Doug Borg <doug.borg@dougborg.org>
2013-10-26 22:31:55 -0600

    Break out the stty stuff into it's own file.
     - I am getting a couple of errors from it right now.

---
+++ home/.bashrc.d/functions/stty.sh
@@ -0,0 +1,4 @@
+# Use ^X instead of ^S to stop control flow so I can reclaim ^S for forward
+# common history search
+stty -ixon
+stty stop ^X
--- home/.bashrc.d/topics/aliases.sh
+++ home/.bashrc.d/topics/aliases.sh
@@ -8,9 +8,4 @@ alias l='ls -CF'
 
 alias c='clear'
 
-# Use ^X instead of ^S to stop control flow so I can reclaim ^S for forward
-# common history search
-stty -ixon
-stty stop ^X
-
 # vim: set ft=sh ts=2 sw=2 tw=0 :
