90739f2dc06ea5506e79f71ec4df0315cf541831
Jason Miller <stormbeta@gmail.com>
2016-05-20 13:48:21 -0600

    Fix git completions on linux

--- home/.bashrc.d/platform/linux.sh
+++ home/.bashrc.d/platform/linux.sh
@@ -6,3 +6,10 @@ eval "$(SHELL=/bin/sh lesspipe)"
 
 # Mix the hub subcommands in with git on linux
 smart-alias git 'hub'
+
+function setjava {
+  local jvmpath="$(find /usr/lib/jvm -maxdepth 1 -type d -name *-$1-*)"
+  export JAVA_HOME="${jvmpath}"
+}
+
+source /usr/share/bash-completion/completions/git
