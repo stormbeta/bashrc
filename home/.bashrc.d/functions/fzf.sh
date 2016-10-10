03e824c738c7460ee2c9630dac1c4b356a5d1914
Jason Miller <stormbeta@gmail.com>
2016-10-10 12:10:11 -0600

    Improve lastpass helper function

--- home/.bashrc.d/functions/fzf.sh
+++ home/.bashrc.d/functions/fzf.sh
@@ -4,7 +4,12 @@
 if command -v fzf 2>&1 > /dev/null; then
   #LastPass fuzzy selection
   function lps {
-    lpass show --password --clip "$(lpass ls --long | fzf | grep -Po '(?<=id: )\d+')"
+    #Ensure login state first; fzf can't handle nested interactive input
+    lpass status --quiet || lpass sync
+    lpass show --password --clip \
+      "$(lpass ls --long | fzf | grep -Po '(?<=id: )\d+')"
   }
+else
+  echo '[WARN]: fzf not installed, skipping fzf-based functions and aliases' 1>&2 >> ~/.bash_warnings
 fi
 
