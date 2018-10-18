939d9080b7d3febc180fd72d7646df803b13aed9
Jason Miller <stormbeta@gmail.com>
2018-10-18 11:15:51 -0600

    Add kubeconfig merge logic

--- home/.bashrc.d/topics/kubernetes.sh
+++ home/.bashrc.d/topics/kubernetes.sh
@@ -1,4 +1,26 @@
+#!/usr/bin/env bash
 # Kubernetes stuff
+
+# Merge kubeconfigs from ~/.kube/config.d automatically so that we can keep clusters separate
+
+function reload-kubeconfigs {
+  # Combine kubeconfigs into ':'-delimited list as KUBECONFIG, which will be auto-merged by kubectl
+  local default="${HOME}/.kube/config"
+  local config_dir="${HOME}/.kube/config.d"
+  local config_paths=""
+  [[ ! -d "$config_dir" ]] && mkdir -p "$config_dir"
+  function __join { local IFS="$1"; shift; echo "$*"; }
+  if [[ -n "$(ls "$config_dir")" ]]; then
+    local configs=( "${config_dir}/"* )
+    config_paths="$(__join : "${configs[@]}")"
+  fi
+  if [[ -f "$default" ]]; then
+    config_paths="${default}:${config_paths}"
+  fi
+  export KUBECONFIG="$config_paths"
+}
+
+reload-kubeconfigs
 
 function k8sh {
   kubectl run -i --tty "$USER-shell-$RANDOM" --image="$1" --restart=Never -- sh
