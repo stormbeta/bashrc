a2d3b610647984297d56c79d9ad7bc9a011c013d
Jason Miller <git@stormbeta.dev>
2025-11-20 18:55:57 -0700

    Auto-start ssh-agent - surprised this wasn't in here already

--- home/.bashrc.d/topics/ssh.sh
+++ home/.bashrc.d/topics/ssh.sh
@@ -3,3 +3,10 @@ __profile "${BASH_SOURCE[0]}"
 if [[ -n "$HOMESHICK" ]] && [[ -d "${HOMESHICK}/bashrc/home/.ssh" ]]; then
   chmod 700 "${HOMESHICK}/bashrc/home/.ssh"
 fi
+
+# Start ssh-agent automatically if not already running
+if command -v ssh-agent &>/dev/null; then
+  if ! pgrep -u "$USER" ssh-agent >/dev/null; then
+      eval "$(ssh-agent -s)"
+  fi
+fi
