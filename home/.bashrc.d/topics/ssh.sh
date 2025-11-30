f7145668e870ac6aaff532e57f862aa27cbb43e4
Jason Miller <git@stormbeta.dev>
2025-11-30 11:23:17 -0700

    Use keychain instead of trying to start ssh-agent ourselves

--- home/.bashrc.d/topics/ssh.sh
+++ home/.bashrc.d/topics/ssh.sh
@@ -4,9 +4,12 @@ if [[ -n "$HOMESHICK" ]] && [[ -d "${HOMESHICK}/bashrc/home/.ssh" ]]; then
   chmod 700 "${HOMESHICK}/bashrc/home/.ssh"
 fi
 
-# Start ssh-agent automatically if not already running
-if command -v ssh-agent &>/dev/null; then
-  if ! pgrep -u "$USER" ssh-agent >/dev/null; then
-      eval "$(ssh-agent -s)"
-  fi
+# Restrict to desktop system, maybe use hostname instead?
+if grep -q Gentoo /etc/os-release; then
+  eval $(keychain --eval --quick --quiet --agents ssh)
 fi
+
+# Start ssh-agent automatically if not already running
+#if ! pgrep -u "$USER" ssh-agent >/dev/null; then
+  #eval "$(ssh-agent -s)"
+#fi
